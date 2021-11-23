64dd69e593c32facc1bf5f99690181cdccc0a758
arsprta <adresseluma@gmail.com>
2021-11-23 19:32:26 +0700

    Pertemuan 6

--- Pertemuan 6/form_login.php
+++ Pertemuan 6/form_login.php
@@ -4,6 +4,7 @@ echo "<h2>Login</h2>
 <table>
 <tr><td>Username</td><td> : <input name='id_user' type='text'></td></tr>
 <tr><td>Password</td><td> : <input name='paswd' type='text'></td></tr>
+<tr><td>Email</td><td> : <input name='email' type='text'></td></tr>
 <tr><td>Captcha<br>
 <img src='captcha.php' /></td><td> : <input name='captcha_code' type='text'></td></tr>
 <tr><td colspan=2><input type='submit' value='LOGIN'></td></tr>
