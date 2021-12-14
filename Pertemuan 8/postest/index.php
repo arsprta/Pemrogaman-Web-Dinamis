fdc40ce70ce554a9d351b5d47a16c5cc7094e682
arsprta <adresseluma@gmail.com>
2021-12-14 21:23:45 +0700

    Pertemuan 8

---
+++ Pertemuan 8/postest/index.php
@@ -0,0 +1,43 @@
+<?php
+// Create database connection using config file
+include_once("koneksi.php");
+// Fetch all users data from database
+$result = mysqli_query($koneksi, "SELECT * FROM mahasiswa ");
+?>
+<html>
+
+<head>
+    <title>Halaman Utama</title>
+</head>
+
+<body>
+    <a href="tambah.php">Tambah Data Baru</a><br /><br />
+    <table width='80%' border=1>
+        <tr>
+            <th>Nim</th>
+            <th>Nama</th>
+            <th>Gender</th>
+            <th>Alamat</th>
+            <th>tgl lahir</th>
+            <th>Jurusan</th>
+            <th>Update</th>
+        </tr>
+        <?php
+        while ($user_data = mysqli_fetch_array($result)) {
+            echo "<tr>";
+            echo "<td>" . $user_data['nim'] . "</td>";
+            echo "<td>" . $user_data['namaMHS'] . "</td>";
+            echo "<td>" . $user_data['jkel'] . "</td>";
+            echo "<td>" . $user_data['alamat'] . "</td>";
+            echo "<td>" . $user_data['tgllhr'] . "</td>";
+            echo "<td>" . $user_data['jurusan'] . "</td>";
+            echo "<td><a href='edit.php?nim=$user_data[nim]'>Edit</a> | <a 
+href='delete.php?nim=$user_data[nim]'>Delete</a></td></tr>";
+        }
+        ?>
+    </table>
+    <br>
+    <a href="laporan.php" target="_BLANK">CETAK DATA MAHASISWA</a>
+</body>
+
+</html>
