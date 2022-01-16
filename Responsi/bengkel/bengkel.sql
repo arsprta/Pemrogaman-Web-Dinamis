f4eba580279e7474d5d06439ae44b02fb32ed7aa
arsprta <adresseluma@gmail.com>
2022-01-16 23:22:37 +0700

    Responsi

--- Responsi/bengkel/bengkel.sql
+++ Responsi/bengkel/bengkel.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 01, 2020 at 01:34 PM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.2.3
+-- Waktu pembuatan: 16 Jan 2022 pada 17.22
+-- Versi server: 10.4.22-MariaDB
+-- Versi PHP: 7.4.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `bengkel_las`
+-- Database: `bengkel`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `details`
+-- Struktur dari tabel `details`
 --
 
 CREATE TABLE `details` (
@@ -37,10 +36,21 @@ CREATE TABLE `details` (
   `qty` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data untuk tabel `details`
+--
+
+INSERT INTO `details` (`id`, `transaction_id`, `product_id`, `name`, `price`, `qty`) VALUES
+(1, 1, 1, 'Ganti Oli', 50000, 1),
+(2, 2, 2, 'Ganti Busi', 25000, 1),
+(3, 3, 4, 'Piston', 450000, 1),
+(4, 4, 1, 'Ganti Oli', 50000, 1),
+(5, 4, 6, 'Ganti Ban', 200000, 1);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `products`
+-- Struktur dari tabel `products`
 --
 
 CREATE TABLE `products` (
@@ -51,10 +61,21 @@ CREATE TABLE `products` (
   `stock` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data untuk tabel `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `price`, `type`, `stock`) VALUES
+(1, 'Ganti Oli', 50000, 'service', NULL),
+(2, 'Ganti Busi', 25000, 'service', NULL),
+(4, 'Piston', 450000, 'sparepart', 4),
+(5, 'Busi', 5000, 'sparepart', 10),
+(6, 'Ganti Ban', 200000, 'service', NULL);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `purchase`
+-- Struktur dari tabel `purchase`
 --
 
 CREATE TABLE `purchase` (
@@ -64,10 +85,18 @@ CREATE TABLE `purchase` (
   `supplier_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data untuk tabel `purchase`
+--
+
+INSERT INTO `purchase` (`id`, `date`, `total`, `supplier_id`) VALUES
+(1, '2022-01-15 18:34:26', 2250000, 1),
+(2, '2022-01-15 20:19:36', 50000, 1);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `purchase_details`
+-- Struktur dari tabel `purchase_details`
 --
 
 CREATE TABLE `purchase_details` (
@@ -78,10 +107,18 @@ CREATE TABLE `purchase_details` (
   `qty` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data untuk tabel `purchase_details`
+--
+
+INSERT INTO `purchase_details` (`id`, `purchase_id`, `product_id`, `price`, `qty`) VALUES
+(1, 1, 4, 450000, 5),
+(2, 2, 5, 5000, 10);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `shop_info`
+-- Struktur dari tabel `shop_info`
 --
 
 CREATE TABLE `shop_info` (
@@ -91,16 +128,16 @@ CREATE TABLE `shop_info` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `shop_info`
+-- Dumping data untuk tabel `shop_info`
 --
 
 INSERT INTO `shop_info` (`id`, `name`, `address`) VALUES
-(1, 'AVS PERKASA', 'Jl. Mencari Cinta Sejati');
+(1, 'Toko Bengkel', 'Jl. Mesin 2 piston');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `suppliers`
+-- Struktur dari tabel `suppliers`
 --
 
 CREATE TABLE `suppliers` (
@@ -110,10 +147,17 @@ CREATE TABLE `suppliers` (
   `telephone` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data untuk tabel `suppliers`
+--
+
+INSERT INTO `suppliers` (`id`, `name`, `address`, `telephone`) VALUES
+(1, 'Adres', 'Tais', '085267231231');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `transactions`
+-- Struktur dari tabel `transactions`
 --
 
 CREATE TABLE `transactions` (
@@ -125,10 +169,20 @@ CREATE TABLE `transactions` (
   `plat` varchar(15) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data untuk tabel `transactions`
+--
+
+INSERT INTO `transactions` (`id`, `type`, `total`, `date`, `customer`, `plat`) VALUES
+(1, 'service', 50000, '2021-12-24 06:38:02', 'Adres', 'BD 4312 H'),
+(2, 'service', 25000, '2022-01-15 10:35:59', 'Lozita', 'BH 4251 H'),
+(3, 'sparepart', 450000, '2022-01-15 18:34:50', NULL, NULL),
+(4, 'service', 250000, '2022-01-15 20:20:37', 'Laila', 'AB 241 C');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
@@ -138,112 +192,112 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `users`
+-- Dumping data untuk tabel `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`) VALUES
-(1, 'admin', '$2y$10$c4WMEmoDy1wmHe/TsH3YJ.aMTHYtDsIyEpNBmly7MuiyAkJ/gh7KS');
+(1, 'admin', '$2y$10$u.8q2VBX3UlOV82kELw8jes.X3haagTu2CkvA9vWI9Fm.3.bB3R2m');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `details`
+-- Indeks untuk tabel `details`
 --
 ALTER TABLE `details`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `products`
+-- Indeks untuk tabel `products`
 --
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `purchase`
+-- Indeks untuk tabel `purchase`
 --
 ALTER TABLE `purchase`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `purchase_details`
+-- Indeks untuk tabel `purchase_details`
 --
 ALTER TABLE `purchase_details`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `shop_info`
+-- Indeks untuk tabel `shop_info`
 --
 ALTER TABLE `shop_info`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `suppliers`
+-- Indeks untuk tabel `suppliers`
 --
 ALTER TABLE `suppliers`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `transactions`
+-- Indeks untuk tabel `transactions`
 --
 ALTER TABLE `transactions`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `details`
+-- AUTO_INCREMENT untuk tabel `details`
 --
 ALTER TABLE `details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `products`
+-- AUTO_INCREMENT untuk tabel `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `purchase`
+-- AUTO_INCREMENT untuk tabel `purchase`
 --
 ALTER TABLE `purchase`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `purchase_details`
+-- AUTO_INCREMENT untuk tabel `purchase_details`
 --
 ALTER TABLE `purchase_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `shop_info`
+-- AUTO_INCREMENT untuk tabel `shop_info`
 --
 ALTER TABLE `shop_info`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `suppliers`
+-- AUTO_INCREMENT untuk tabel `suppliers`
 --
 ALTER TABLE `suppliers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `transactions`
+-- AUTO_INCREMENT untuk tabel `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
